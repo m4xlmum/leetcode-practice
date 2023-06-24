74ae8dc1bda73dc5fb3aa1fe9c1e165009e09914
m4xlmum <2994016494@qq.com>
2023-06-25 01:45:37 +0800

    更新一题

--- leetcode_3.java
+++ leetcode_3.java
@@ -5,7 +5,28 @@ import java.util.*;
 public class leetcode_3 {
     // solution函数
     public int lengthOfLongestSubstring(String s) {
+        // 创建HashMap用来存储最后一个字符的位置
+        HashMap<Character, Integer> map = new HashMap<>();
+        // 最长字符串的开头和结尾处
+        int start = 0, end = 0;
+        // 结果
+        int ans = 0;
+        // 获取输入的字符串的长度
+        int n = s.length();
+        for (; end < n; end++) {
+            // 获取当前末尾字符
+            char ch = s.charAt(end);
+            // 字符出现重复
+            if (map.containsKey(ch)) {
+                // 更新开始位置
+                start = map.get(ch);
+            }
+            // 更新最长字符串的长度
+            ans = Math.max(ans, end - start + 1);
+            // 更新字符位置，end+1作为下一轮的start确保下一串字符串不会出现重复
+            map.put(ch, end + 1);
+        }
 
-        return 1;
+        return ans;
     }
 }
